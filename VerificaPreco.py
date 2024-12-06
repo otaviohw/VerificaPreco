ad513eca52ba033d0c1bbefc529f92c1c96083d5
Otávio Hellmann Wiemes <61704704+otaviohw@users.noreply.github.com>
2024-12-06 19:56:23 -0300

    verificar a cada 30 minutos

--- VerificaPreco.py
+++ VerificaPreco.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 url = "https://www.granado.com.br/granado/perfume-granado-esplendor-75ml?queryID=882b62ce96fe32e1f0c61c34c4f5b99a&objectID=5965&indexName=prd_granado_products"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+while True:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-price = soup.find('span', class_='price').text.strip()
-print(f"Preço: {price}")
+    price = soup.find('span', class_='price').text.strip()
+    print(f"Preço: {price}")
+    time.sleep(1800)
